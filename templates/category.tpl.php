b27ee62804e491856ee12fcaea22382e57f17bb2
Ben Caldwell <bcaldwell@qualtim.com>
2018-11-30 10:46:35 -0600

    Removed redundant title attribute

--- templates/category.tpl.php
+++ templates/category.tpl.php
@@ -1,11 +1,14 @@
 <li class="<?php echo ($no_children ? 'no-children' : 'children') ?>" >
-<a href="<?php echo $url; ?>" title="<?php echo $short_description; ?>">
-<?php echo $term_name; ?></a> 
-(<?php echo $node_count; ?>)
-<?php if (!empty($short_description)): ?>
-	- <?php echo $short_description; ?>
-<?php endif; ?>
-<?php if (isset($edit_term_link)): ?>
-	<?php print $edit_term_link; ?>
+<a href="<?php echo $url; ?>">
+<?php if ($node_count != 0): ?>
+	<?php echo $term_name; ?></a> 
+	(<?php echo $node_count; ?>)
+	<?php if (!empty($short_description)): ?>
+		- <?php echo $short_description; ?>
+	<?php endif; ?>
+
+	<?php if (isset($edit_term_link)): ?>
+		<?php print $edit_term_link; ?>
+	<?php endif; ?> 
 <?php endif; ?> 
 </li>
